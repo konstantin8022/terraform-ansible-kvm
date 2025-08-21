f0ec040980a21d63029ca1f9bd48faca5d80d64e
konstantin8022 <konstantinstrelcov82@gmail.com>
2025-08-21 15:59:59 +0300

    go

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,8 @@ output "ip" {
   value = libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]
 }
 
+#go
+
 output "url" {
   value = "http://${libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]}"
 }
